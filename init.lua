70eb06a304c540af390b69f0d6a81b154c8fecfe
sagmansercan <sagmansercan@gmail.com>
2024-05-18 18:20:02 +0300

    minimize mini

--- init.lua
+++ init.lua
@@ -462,24 +462,6 @@ require('lazy').setup({
     {
         'echasnovski/mini.nvim',
         config = function()
-            -- Better Around/Inside textobjects
-            --
-            -- Examples:
-            --  - va)  - [V]isually select [A]round [)]paren
-            --  - yinq - [Y]ank [I]nside [N]ext [']quote
-            --  - ci'  - [C]hange [I]nside [']quote
-            require('mini.ai').setup { n_lines = 500 }
-
-            -- Add/delete/replace surroundings (brackets, quotes, etc.)
-            --
-            -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
-            -- - sd'   - [S]urround [D]elete [']quotes
-            -- - sr)'  - [S]urround [R]eplace [)] [']
-            require('mini.surround').setup()
-
-            -- Simple and easy statusline.
-            --  You could remove this setup call if you don't like it,
-            --  and try some other statusline plugin
             local statusline = require 'mini.statusline'
             statusline.setup { use_icons = vim.g.have_nerd_font }
 
