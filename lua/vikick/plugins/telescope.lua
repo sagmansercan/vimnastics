e09e90a4e8d3ad53965a092c81dd3600cf59d779
sagmansercan <sagmansercan@gmail.com>
2024-05-20 01:00:54 +0300

    revisit keymaps

--- lua/vikick/plugins/telescope.lua
+++ lua/vikick/plugins/telescope.lua
@@ -20,7 +20,7 @@ return {
                 mappings = {
                     i = {
                         -- ['<c-enter>'] = 'to_fuzzy_refine',
-                        -- ['<ESC>'] = actions.close,
+                        ['<C-c>'] = actions.close,
                         -- ['<Down>'] = actions.cycle_history_next,
                         -- ['<Up>'] = actions.cycle_history_prev,
                         ['<C-j>'] = actions.move_selection_next,
@@ -47,42 +47,53 @@ return {
         pcall(require('telescope').load_extension, 'ui-select')
 
         local builtin = require 'telescope.builtin'
-        vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
-        vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
-        vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
+        vim.keymap.set('n', '<leader>th', builtin.help_tags, { desc = '[S]earch [H]elp' })
+        vim.keymap.set('n', '<leader>tk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
+        vim.keymap.set('n', '<leader>f', builtin.find_files, { desc = '[S]earch [F]iles' })
+        vim.keymap.set('n', '<C-f>', builtin.find_files, { desc = '[S]earch [F]iles' })
         -- vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
-        vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
-        vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
-        vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
-        vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
-        vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
-        vim.keymap.set('n', '<leader>so', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
+        -- vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
+        vim.keymap.set('n', '<leader>g', builtin.live_grep, { desc = '[S]earch by [G]rep' })
+        vim.keymap.set('n', '<leader>td', function()
+            -- custom opts
+            builtin.diagnostics(require('telescope.themes').get_ivy {
+                previewer = false,
+                layout_config = {
+                    -- width = 0.7,
+                },
+                sorting_strategy = 'ascending',
+                line_width = 0.7,
+            })
+        end, { desc = '[S]earch [D]iagnostics' })
+        -- vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
+        vim.keymap.set('n', '<leader>o', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
         vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
-        vim.keymap.set('n', '<leader>ss', function()
+        vim.keymap.set('n', '<leader>s', function()
             --[[
-        get_dropdown returned config ref
-        {
-          border = true,
-          borderchars = {
-            preview = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" },
-            prompt = { "─", "│", " ", "│", "╭", "╮", "│", "│" },
-            results = { "─", "│", "─", "│", "├", "┤", "╯", "╰" }
-          },
-          layout_config = {
-            height = <function 1>,
-            preview_cutoff = 1,
-            width = <function 2>
-          },
-          layout_strategy = "center",
-          previewer = false,
-          results_title = false,
-          sorting_strategy = "ascending",
-          theme = "dropdown",
-          winblend = 10
-        }
-        --]]
+            get_dropdown returned config ref
+            {
+              border = true,
+              borderchars = {
+                preview = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" },
+                prompt = { "─", "│", " ", "│", "╭", "╮", "│", "│" },
+                results = { "─", "│", "─", "│", "├", "┤", "╯", "╰" }
+              },
+              layout_config = {
+                height = <function 1>,
+                preview_cutoff = 1,
+                width = <function 2>
+              },
+              layout_strategy = "center",
+              previewer = false,
+              results_title = false,
+              sorting_strategy = "ascending",
+              theme = "dropdown",
+              winblend = 10
+            }
+            --]]
+
             builtin.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
-                winblend = 1,
+                -- winblend = 1,
                 previewer = false,
                 layout_config = {
                     width = 0.7,
@@ -90,17 +101,17 @@ return {
                 sorting_strategy = 'descending',
             })
         end, { desc = '[/] Fuzzily search in current buffer' })
-        vim.keymap.set(
-            'n',
-            '<leader>s/',
-            function()
-                builtin.live_grep {
-                    grep_open_files = true,
-                    prompt_title = 'Live Grep in Open Files',
-                }
-            end,
-            { desc = '[S]earch [/] in Open Files' }
-        )
-        vim.keymap.set('n', '<leader>sn', function() builtin.find_files { cwd = vim.fn.stdpath 'config' } end, { desc = '[S]earch [N]eovim files' })
+        -- vim.keymap.set(
+        --     'n',
+        --     '<leader>s/',
+        --     function()
+        --         builtin.live_grep {
+        --             grep_open_files = true,
+        --             prompt_title = 'Live Grep in Open Files',
+        --         }
+        --     end,
+        --     { desc = '[S]earch [/] in Open Files' }
+        -- )
+        -- vim.keymap.set('n', '<leader>sn', function() builtin.find_files { cwd = vim.fn.stdpath 'config' } end, { desc = '[S]earch [N]eovim files' })
     end,
 }
