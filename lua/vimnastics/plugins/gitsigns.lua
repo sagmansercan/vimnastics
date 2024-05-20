ef62f769066add16871aa91e9367a30172a051af
sagmansercan <sagmansercan@gmail.com>
2024-05-20 08:32:44 +0300

    update gitsign keymaps

--- lua/vimnastics/plugins/gitsigns.lua
+++ lua/vimnastics/plugins/gitsigns.lua
@@ -18,14 +18,12 @@ return {
                     vim.keymap.set(mode, l, r, opts)
                 end
 
-                -- normal mode
-                map('n', '<leader>eR', gitsigns.reset_buffer, { desc = 'git [R]eset buffer' })
-                map('n', '<leader>eb', gitsigns.blame_line, { desc = 'git [b]lame line' })
-                map('n', '<leader>ed', gitsigns.diffthis, { desc = 'git [d]iff against index' })
-                map('n', '<leader>eD', function() gitsigns.diffthis '@' end, { desc = 'git [D]iff against last commit' })
-                -- Toggles
-                map('n', '<leader>etb', gitsigns.toggle_current_line_blame, { desc = '[T]oggle git show [b]lame line' })
-                map('n', '<leader>etD', gitsigns.toggle_deleted, { desc = '[T]oggle git show [D]eleted' })
+                map('n', '<leader>erb', gitsigns.reset_buffer, { desc = 'git geset buffer' })
+                map('n', '<leader>ebl', gitsigns.blame_line, { desc = 'git blame line' })
+                map('n', '<leader>edi', gitsigns.diffthis, { desc = 'git diff against index' })
+                map('n', '<leader>edl', function() gitsigns.diffthis '@' end, { desc = 'git diff against last commit' })
+                map('n', '<leader>etb', gitsigns.toggle_current_line_blame, { desc = 'toggle git show blame line' })
+                map('n', '<leader>etd', gitsigns.toggle_deleted, { desc = 'toggle git show deleted' })
             end,
         },
     },
