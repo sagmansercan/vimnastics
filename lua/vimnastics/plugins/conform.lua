4bff7b79aa55950dc428975c5e88bb99b3ff0aaf
sagmansercan <sagmansercan@gmail.com>
2024-05-21 12:04:57 +0300

    disable java formatting on save

--- lua/vimnastics/plugins/conform.lua
+++ lua/vimnastics/plugins/conform.lua
@@ -12,7 +12,7 @@ return {
     opts = {
         notify_on_error = false,
         format_on_save = function(bufnr)
-            local disable_filetypes = { c = true, cpp = true }
+            local disable_filetypes = { c = true, cpp = true, java = true }
             return {
                 timeout_ms = 500,
                 lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
