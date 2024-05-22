1f638bb89371b6c52524d7987ed836d11f596a98
sagmansercan <sagmansercan@gmail.com>
2024-05-22 12:30:01 +0300

    try copilot temparature setting

--- lua/vimnastics/plugins/copilot.lua
+++ lua/vimnastics/plugins/copilot.lua
@@ -55,12 +55,10 @@ return {
         },
         copilot_node_command = 'node',
         server_opts_overrides = {
-            -- trace = 'verbose',
             settings = {
-                -- advanced = {
-                --     listCount = 10, -- #completions for panel
-                --     inlineSuggestCount = 3, -- #completions for getCompletions
-                -- },
+                advanced = {
+                    temperature = 0.5, -- 0-1, 0=conservative, 1=creative
+                },
             },
         },
     },
