4630b086fa5459d5ee1b3b4f7ebab5bc99da84c3
sagmansercan <sagmansercan@gmail.com>
2024-05-30 17:34:50 +0300

    disable markdown lint

--- lua/vimnastics/plugins/lint.lua
+++ lua/vimnastics/plugins/lint.lua
@@ -6,7 +6,7 @@ return {
             local lint = require 'lint'
             lint.linters_by_ft = lint.linters_by_ft or {}
 
-            lint.linters_by_ft['markdown'] = { 'markdownlint' }
+            -- lint.linters_by_ft['markdown'] = { 'markdownlint' }
 
             -- default
             -- {
@@ -35,7 +35,9 @@ return {
             local lint_augroup = vim.api.nvim_create_augroup('lint', { clear = true })
             vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWritePost', 'InsertLeave' }, {
                 group = lint_augroup,
-                callback = function() require('lint').try_lint() end,
+                callback = function()
+                    require('lint').try_lint()
+                end,
             })
         end,
     },
