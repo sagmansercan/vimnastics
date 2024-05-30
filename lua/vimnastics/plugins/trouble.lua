d71a74685d782241bab95ac722da1b221d63e3b1
sagmansercan <sagmansercan@gmail.com>
2024-05-30 17:35:19 +0300

    update trouble toggle keymaps

--- lua/vimnastics/plugins/trouble.lua
+++ lua/vimnastics/plugins/trouble.lua
@@ -3,12 +3,12 @@ return {
     branch = 'dev', -- IMPORTANT!
     keys = {
         {
-            '<leader>xx',
+            '<leader>trw',
             '<cmd>Trouble diagnostics toggle<cr>',
             desc = 'Diagnostics (Trouble)',
         },
         {
-            '<leader>xX',
+            '<leader>trc',
             '<cmd>Trouble diagnostics toggle filter.buf=0<cr>',
             desc = 'Buffer Diagnostics (Trouble)',
         },
