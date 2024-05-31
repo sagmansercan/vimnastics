99e9d2335ce28f6d399a342f123540f3de7bdcd0
sagmansercan <sagmansercan@gmail.com>
2024-05-31 14:20:22 +0300

    add buffer to completion sources

--- lua/vimnastics/plugins/cmp.lua
+++ lua/vimnastics/plugins/cmp.lua
@@ -97,6 +97,7 @@ return {
                 { name = 'nvim_lsp' },
                 { name = 'luasnip' },
                 { name = 'path' },
+                { name = 'buffer' },
             },
             window = {
                 completion = {
